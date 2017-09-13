6057685517f4e30b5ea6f4cfda11e8ac50fe6893
Grzegorz Uriasz <gorbak25@gmail.com>
2017-09-13 20:01:46 +0200

    Using generators for data augumentation and running the models on smaller instances

--- models/cloud_run.sh
+++ models/cloud_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 neptune data upload ../common/common.py;
-neptune send --config ../common/neptune-cloud.yaml --input tryponet_set-test --input common.py --environment keras-2.0-cpu-py3 --worker gcp-small main.py;
+neptune send --config ../common/neptune-cloud.yaml --input tryponet_set --input common.py --environment keras-2.0-cpu-py3 --worker gcp-small main.py;
 
