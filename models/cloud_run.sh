1a3920ea30b94a4c37b0b04d0e3a9f9ad5567272
Artur Puzio <cytadela8@interia.pl>
2017-09-14 14:08:15 +0200

    Better cloud_run

--- models/cloud_run.sh
+++ models/cloud_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 neptune data upload ../common/common.py;
-neptune send --config ../common/neptune-cloud.yaml --input tryponet_set-test --input common.py --environment keras-2.0-gpu-py3 --worker gcp-gpu-large main.py;
+neptune send --config ../common/neptune-cloud.yaml --input common.py --environment keras-2.0-gpu-py3 --worker gcp-gpu-large main.py "$@"
 
