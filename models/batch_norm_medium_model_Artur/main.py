56e8156365807a43511b1f4d9a712960c0fd7c31
Artur Puzio <cytadela8@interia.pl>
2017-09-15 12:37:38 +0200

    Dodano model batch_norm_medium

---
+++ models/batch_norm_medium_model_Artur/main.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.insert(0, "/input/") #for cloud
+sys.path.insert(0, "../common/") #for local
+import common
+
+from keras.models import Sequential
+from keras.layers import *
+
+model = Sequential()
+model.add(Conv2D(16, (3, 3), padding='same',
+          input_shape=(common.resolution_x, common.resolution_y, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(24, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(24, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.3))
+model.add(BatchNormalization())
+model.add(GlobalAveragePooling2D())
+model.add(Dense(2))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+common.experiment(model)
