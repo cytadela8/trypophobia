1e9f60fd8eee522ac7326bbf24b43b01991cec8e
Artur Puzio <cytadela8@interia.pl>
2017-09-15 13:07:16 +0200

    Better models docs

--- models/get_model_complexity.sh
+++ models/get_model_complexity.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-#Usage: ./get_model_complexity.sh [path_to_main.py_of_the_model]
+# This script reports number of parameter in model defined in .py script with path
+
+if [ "$#" -ne 1 ]; then
+    echo Usage: $0 path_to_main.py_of_the_model
+    exit
+fi
+
 
 tail -n +$(nl $1 | grep "import common" | awk '{print $1+1}') $1 | sed '/common.experiment(.*)/ s/)/.summary()/' | sed 's/^common.experiment(//' | sed 's/common.resolution_./256/g' | python3 2>&1 | grep params
